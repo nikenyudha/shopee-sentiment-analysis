28fce08ed04d800268486e9cde2d511059252ae5
Nikenyudha <Nikenyudhaa22@gmail.com>
2026-06-26 17:14:58 +0700

    git commit -m "docs: update cleaning_data_shopee"

--- cleaning_data_shopee.py
+++ cleaning_data_shopee.py
@@ -1,28 +1,28 @@
 import pandas as pd
 import re
 
-# 1. Load data yang sudah di-scrape tadi
+# 1. Load data y
 df = pd.read_csv('reviews_shopee.csv')
 
 def clean_text(text):
-    # Ubah ke string dan kecilkan huruf
+    # Change to string and lower case
     text = str(text).lower()
-    # Hapus URL/Link
+    # Remove URL/Link
     text = re.sub(r'http\S+|www\S+|https\S+', '', text, flags=re.MULTILINE)
-    # Hapus mention (@) dan hashtag (#)
+    # Remove mention (@) and hashtag (#)
     text = re.sub(r'@\w+|#\w+', '', text)
-    # Hapus angka dan tanda baca
+    # Remove numbers and punctuation
     text = re.sub(r'[^\w\s]', '', text)
     text = re.sub(r'\d+', '', text)
-    # Hapus spasi berlebih
+    # Remove extra whitespace
     text = text.strip()
     return text
 
-# 2. Jalankan fungsi pembersih
+# 2. Run the cleaning function
 df['content_cleaned'] = df['content'].apply(clean_text)
 
-# 3. Buat Label Sentimen Sederhana (Pseudo-labeling)
-# Skor 4-5 = Positif (1), Skor 1-2 = Negatif (0), Skor 3 = Netral ( abaikan dulu agar fokus)
+# 3. Create Simple Sentiment Labels (Pseudo-labeling)
+# Score 4-5 = Positive (1), Score 1-2 = Negative (0), Score 3 = Neutral (ignore this for now to focus)
 def create_label(score):
     if score >= 4:
         return 1
@@ -33,10 +33,10 @@ def create_label(score):
 
 df['label'] = df['score'].apply(create_label)
 
-# Hapus yang netral agar model belajar perbedaan yang kontras
+# Remove the neutral so that the model learns contrasting differences.
 df = df.dropna(subset=['label'])
 
-# 4. Simpan data yang sudah bersih
+# 4. Save the clean data to a new CSV file
 df[['content_cleaned', 'label']].to_csv('shopee_reviews_cleaned.csv', index=False)
 
 print("Data sudah bersih dan siap diolah!")
